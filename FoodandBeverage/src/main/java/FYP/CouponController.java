85483f95f09037e9aa8da7db70738c0d62e88d58
22011907-Sherwayne-HengJunYang <22011907@myrp.edu.sg>
2024-05-08 15:12:38 +0800

    Able to delete vendors and coupons

--- FoodandBeverage/src/main/java/FYP/CouponController.java
+++ FoodandBeverage/src/main/java/FYP/CouponController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
@@ -67,6 +68,13 @@ public class CouponController {
         calendar.add(Calendar.DAY_OF_MONTH, 30); // Adds 30 days to the issue date
         return calendar.getTime();
     }
-
+    
+    @GetMapping("/coupons/delete/{id}")
+    public String deleteItem(@PathVariable("id") Integer id) {
+    	
+    	CouponRepository.deleteById(id);
+    	
+    	return "redirect:/coupons";
+    }
    
 }
